18c03ae6f16d209b5c3e885e997826eb1828eba5
andrew <porcherface@gmail.com>
2023-11-18 12:54:30 +0100

    chores: comment and noindent

--- public_html/index.php
+++ public_html/index.php
@@ -7,24 +7,27 @@ $page =  basename($_SERVER['PHP_SELF']);
 # loads the required queries from top controller
 # using $page
 require('../app/controller/top.php');
-
 # HTML START --------
+
+require('layout/intestazione.php');
 require('layout/header.php');
 ?>
 
+<!-- content -->
+<div class="wrapper">
 
-    <div class="wrapper">
+    <h1>Welcome to my PHP page!</h1>
+    <p>This is a simple PHP page.</p>
 
-        <h1>Welcome to my PHP page!</h1>
-        <p>This is a simple PHP page.</p>
+</div >  
 
-    </div>
+
 
 <?php
 require('layout/footer.php');
 ?>
-<!-- end html-->
 
+<!-- end html-->
 <?php # not closing this is common in php
 require('../app/controller/bottom.php');
 
